8729e4897a526f168f5a8081de075af1f7016b3c
Beartil Rajan <229802592+beartilrajan@users.noreply.github.com>
2025-12-11 17:01:01 +0000

    Calculator display added

--- main.js
+++ main.js
@@ -3,15 +3,22 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-    return a-b;
+    return a - b;
 }
 
-function multiply(a,b) {
-    return a*b;
-    
+function multiply(a, b) {
+    return a * b;
+
 }
 
-function divide(a,b) {
-    return a/b;
-    
+function divide(a, b) {
+    return a / b;
+
+}
+
+
+
+function pressNumber(num) {
+    let display = document.getElementById("display")
+    display.value += num;
 }
