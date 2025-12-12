b0b91241aa928e1584242d1aa2a8cb3416352937
Beartil Rajan <229802592+beartilrajan@users.noreply.github.com>
2025-12-12 16:36:34 +0000

    The working calculator is completed

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
-
-let previousInput = "";
-let operationInput = "";
-let currentInput = "";
+previousInput = "";
+inputOperator = "";
+currentInput = "";
 
 function pressNumber(num) {
     currentInput += num;
+    document.getElementById("display").value = `${previousInput} ${inputOperator} ${currentInput}`;
 }
 
-function getOperator(){
-    
+
+function getOperator(operator) {
+    if (previousInput !== "" && currentInput !== "") {
+        calculater()
+    }
+    inputOperator = operator
+    previousInput = currentInput;
+    currentInput = "";
+    document.getElementById('display').value = `${previousInput} ${inputOperator}`
+
 }
 
-function calculate(){
-    
+function calculater() {
+    let result;
+    let a = parseFloat(previousInput);
+    let b = parseFloat(currentInput);
+    switch (inputOperator) {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break;
+
+        case "*":
+            result = a * b;
+            break;
+        case "/":
+            if (b == 0) {
+                alert("devide by zero not allowd")
+                clearDisplay();
+                return;
+            }
+            else {
+                result = a / b;
+                break;
+            }
+        default:
+            return;
+    }
+
+    currentInput = result.toString();
+    previousInput = ""
+    inputOperator = "";
+
+    document.getElementById("display").value = currentInput;
 }
 
 function clearDisplay() {
+    previousInput = "";
+    inputOperator = "";
     currentInput = "";
-    operationInpuut = "";
-    nextInput = "";
-    display.value = "";
-}
-
-
-function calculate() {
-
+    document.getElementById("display").value = "";
 }
